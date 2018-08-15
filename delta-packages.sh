f1e099f6fb4e2a14f0acae993145d5ef260ef8f6
kate ross-smith <kate.ross-smith@ovoenergy.com>
2018-08-15 15:29:04 +0100

    idiot

--- delta-packages.sh
+++ delta-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-awk 'FNR==NR {a[$0]++; next} !a[$0]' ~/noobjaro/packages-aur.new.log ~/noobjaro/packages-aur.old.log | install -D /dev/stdout ~/noobjaro/packages-aur.delta.log
-awk 'FNR==NR {a[$0]++; next} !a[$0]' ~/noobjaro/packages.new.log ~/noobjaro/packages.old.log | install -D /dev/stdout ~/noobjaro/packages.delta.log
+awk 'FNR==NR {a[$0]++; next} !a[$0]' ~/noobjaro/packages-aur.new.log ~/noobjaro/packages-aur.old.log | install -D /dev/stdin ~/noobjaro/packages-aur.delta.log
+awk 'FNR==NR {a[$0]++; next} !a[$0]' ~/noobjaro/packages.new.log ~/noobjaro/packages.old.log | install -D /dev/stdin ~/noobjaro/packages.delta.log
